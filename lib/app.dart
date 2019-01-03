6daf207624515c03229267621d684a7d7b99c5dd
CaramelDunes <dev@carameldunes.fr>
2019-01-03 13:25:56 +0100

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:nyanya_rocket/app.dart';
+
+void main() => runApp(App());
