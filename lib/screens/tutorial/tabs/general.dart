0599a1f0f371f4a6f3dda99b84425e63ee4ff09e
CaramelDunes <dev@carameldunes.fr>
2019-03-21 23:26:57 +0100

    Begin tutorial.

--- lib/screens/tutorial/tabs/general.dart
+++ lib/screens/tutorial/tabs/general.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nyanya_rocket/widgets/game_view/entities_drawer.dart';
+import 'package:nyanya_rocket/widgets/game_view/tiles_drawer.dart';
+import 'package:nyanya_rocket_base/nyanya_rocket_base.dart';
 
 class General extends StatelessWidget {
   @override
@@ -6,8 +9,136 @@ class General extends StatelessWidget {
     return ListView(
       padding: const EdgeInsets.all(8),
       children: <Widget>[
-        Text('Synopsis', style: Theme.of(context).textTheme.title),
-        Text('Board', style: Theme.of(context).textTheme.subtitle)
+        Text(
+          'Entities',
+          style: Theme.of(context).textTheme.headline,
+        ),
+        Card(
+          child: Container(
+              height: 76,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: EntitiesDrawer.entityView(
+                        EntityType.Mouse, Direction.Right),
+                  )),
+                  Flexible(
+                    flex: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Mouse'),
+                    ),
+                  ),
+                ],
+              )),
+        ),
+        Card(
+          child: Container(
+              height: 76,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: EntitiesDrawer.entityView(
+                        EntityType.Cat, Direction.Down),
+                  )),
+                  Flexible(
+                    flex: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Cat'),
+                    ),
+                  ),
+                ],
+              )),
+        ),
+        Text("""
+An entity walks straight until it encounters an arrow or a wall.
+"""),
+        Divider(),
+        Text(
+          'Tiles',
+          style: Theme.of(context).textTheme.headline,
+        ),
+        Card(
+          child: Container(
+              height: 76,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TilesDrawer.tileView(Pit()),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Black Hole (technically a square though...)'),
+                  )
+                ],
+              )),
+        ),
+        Card(
+          child: Container(
+              height: 76,
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                      flex: 0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TilesDrawer.tileView(
+                            Rocket(player: PlayerColor.Blue)),
+                      )),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Rocket'),
+                    ),
+                  ),
+                ],
+              )),
+        ),
+        Card(
+          child: Container(
+              height: 76,
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TilesDrawer.tileView(Arrow(
+                          player: PlayerColor.Blue, direction: Direction.Up)),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Arrow'),
+                    ),
+                  ),
+                ],
+              )),
+        ),
+        Divider(),
+        Text(
+          'Arrow placement',
+          style: Theme.of(context).textTheme.headline,
+        ),
+        Text('Arrows can be drag n\' dropped...'),
+        Container(
+          height: 100,
+          child: Placeholder(),
+        ),
+        Text('... or swiped'),
+        Container(
+          height: 100,
+          child: Placeholder(),
+        )
       ],
     );
   }
--- lib/screens/tutorial/tabs/puzzle.dart
+++ lib/screens/tutorial/tabs/puzzle.dart
@@ -3,6 +3,23 @@ import 'package:flutter/material.dart';
 class Puzzle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const SizedBox.shrink();
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        Text("""
+A Puzzle game is won when every mice has reached a rocket.
+You have a restricted amount of arrows to achieve that goal.
+
+Note that you cannot place or remove an arrow after pressing the play button; you'll need to reset the level first.
+
+You'll lose if:
+  • A cat enters a rocket before every mice is safe.
+  • A mouse gets eaten by a cat.
+  • A mouse falls into a black hole.
+
+You'll be awarded a star if you complete a level without using all the available arrows.
+"""),
+      ],
+    );
   }
 }
