ae8a04101141df94cef602703d7cdd578d143c8e
CaramelDunes <dev@carameldunes.fr>
2020-05-30 23:09:55 +0200

    Give a const constructor to the Contributors widget.

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Contributors extends StatelessWidget {
-  static const List<String> _list = const [
+  static const List<String> _list = [
     'CaramelDunes',
     'brouxco', // Early testing
     'GuilloteauQ', // Early testing
     'chauvean', // Discovered null shared pref bug
     'Savony', // German translation
   ];
+
+  const Contributors({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
