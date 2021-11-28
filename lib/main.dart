f7f6b5760123bac630b4ffc62af50e9754a20311
CaramelDunes <dev@carameldunes.fr>
2021-11-28 21:53:26 +0100

    Reorder things around in main.

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,22 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
-
-import 'package:nyanya_rocket/app.dart';
-import 'package:nyanya_rocket/config.dart';
-import 'package:nyanya_rocket/warm_up_flare.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'app.dart';
+import 'config.dart';
 import 'models/stores/file_named_data_store.dart';
 import 'models/stores/prefs_named_data_store.dart';
 import 'models/stores/named_data_store.dart';
 import 'services/firestore/firestore_service.dart';
+import 'warm_up_flare.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await warmUpFlare();
 
-  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-
-  if (kIsWeb) {
-    NamedDataStorage.active = PrefsDataStore(sharedPreferences);
-  } else {
-    NamedDataStorage.active = FileNamedDataStorage();
-  }
+  final SharedPreferences sharedPreferences =
+      await SharedPreferences.getInstance();
 
   // When the native plugin in available,
   // Firebase will initialize using the embedded config JSON.
@@ -41,6 +35,9 @@ Future<void> main() async {
 
   if (kIsWeb) {
     await FirebaseAuth.instance.setPersistence(Persistence.LOCAL);
+    NamedDataStorage.active = PrefsDataStore(sharedPreferences);
+  } else {
+    NamedDataStorage.active = FileNamedDataStorage();
   }
 
   runApp(App(
