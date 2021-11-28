49c75a263f4c8cfd7e30fc72f46d450ee8f7f743
CaramelDunes <dev@carameldunes.fr>
2021-11-28 15:44:30 +0100

    Move idToken fetch one level above.

--- lib/screens/multiplayer/multiplayer.dart
+++ lib/screens/multiplayer/multiplayer.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:nyanya_rocket/blocs/multiplayer_queue.dart';
@@ -38,76 +36,57 @@ class _MultiplayerState extends State<Multiplayer>
   Widget build(BuildContext context) {
     return Consumer2<Region, User>(builder:
         (BuildContext context, Region region, User user, Widget? child) {
-      return BarRailTabs(
-          title: NyaNyaLocalizations.of(context).multiplayerTitle +
-              ' (${region.label})',
-          appBarActions: [
-            IconButton(
-              icon: const Icon(Icons.settings),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (BuildContext context) {
-                  return const Settings();
-                }));
-              },
-            ),
-            PopupMenuButton<String>(
-              itemBuilder: (BuildContext context) {
-                return [
-                  PopupMenuItem(
-                    child:
-                        Text(NyaNyaLocalizations.of(context).deviceDuelLabel),
-                    value: 'device',
+      return FutureBuilder(
+          future: user.idToken(),
+          builder: (context, AsyncSnapshot<String?> snapshot) {
+            return BarRailTabs(
+                title: NyaNyaLocalizations.of(context).multiplayerTitle +
+                    ' (${region.label})',
+                appBarActions: [
+                  IconButton(
+                    icon: const Icon(Icons.settings),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (BuildContext context) {
+                        return const Settings();
+                      }));
+                    },
                   ),
-                  PopupMenuItem(
-                    child: Text(
-                        NyaNyaLocalizations.of(context).lanMultiplayerLabel),
-                    value: 'lan',
-                  )
-                ];
-              },
-              onSelected: (value) {
-                if (value == 'lan') {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return const LanMultiplayerSetup();
-                  }));
-                } else if (value == 'device') {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return const DeviceDuelSetup();
-                  }));
-                }
-              },
-            )
-          ],
-          tabs: [
-            BarRailTab(
-              icon: const FaIcon(FontAwesomeIcons.userFriends),
-              label: NyaNyaLocalizations.of(context).duelLabel,
-              content: FutureBuilder(
-                  future: user.idToken(),
-                  builder: (context, AsyncSnapshot<String?> snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
-                    } else if (!snapshot.hasData) {
-                      return Center(
+                  PopupMenuButton<String>(
+                    itemBuilder: (BuildContext context) {
+                      return [
+                        PopupMenuItem(
+                          child: Text(
+                              NyaNyaLocalizations.of(context).deviceDuelLabel),
+                          value: 'device',
+                        ),
+                        PopupMenuItem(
                           child: Text(NyaNyaLocalizations.of(context)
-                              .unauthenticatedError));
-                    }
-
-                    return QueueAndLeaderboard(
-                      queueType: QueueType.duels,
-                      displayName: user.displayName ?? '',
-                      idToken: snapshot.data!,
-                      masterServerHostname: region.masterServerHostname,
-                    );
-                  }),
-            ),
-            BarRailTab(
-                icon: const FaIcon(FontAwesomeIcons.users),
-                label: NyaNyaLocalizations.of(context).fourPlayersLabel,
-                content: FutureBuilder(
-                    future: user.idToken(),
-                    builder: (context, AsyncSnapshot<String?> snapshot) {
+                              .lanMultiplayerLabel),
+                          value: 'lan',
+                        )
+                      ];
+                    },
+                    onSelected: (value) {
+                      if (value == 'lan') {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return const LanMultiplayerSetup();
+                        }));
+                      } else if (value == 'device') {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return const DeviceDuelSetup();
+                        }));
+                      }
+                    },
+                  )
+                ],
+                tabs: [
+                  BarRailTab(
+                    icon: const FaIcon(FontAwesomeIcons.userFriends),
+                    label: NyaNyaLocalizations.of(context).duelLabel,
+                    content: Builder(builder: (context) {
                       if (snapshot.connectionState == ConnectionState.waiting) {
                         return const Center(child: CircularProgressIndicator());
                       } else if (!snapshot.hasData) {
@@ -117,32 +96,56 @@ class _MultiplayerState extends State<Multiplayer>
                       }
 
                       return QueueAndLeaderboard(
-                        queueType: QueueType.fourPlayers,
+                        queueType: QueueType.duels,
                         displayName: user.displayName ?? '',
                         idToken: snapshot.data!,
                         masterServerHostname: region.masterServerHostname,
                       );
-                    })),
-            BarRailTab(
-                icon: const FaIcon(FontAwesomeIcons.peopleArrows),
-                label: NyaNyaLocalizations.of(context).friendDuelLabel,
-                content: FutureBuilder(
-                    builder: (context, AsyncSnapshot<String?> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (!snapshot.hasData) {
-                    return Center(
-                        child: Text(NyaNyaLocalizations.of(context)
-                            .unauthenticatedError));
-                  }
+                    }),
+                  ),
+                  BarRailTab(
+                      icon: const FaIcon(FontAwesomeIcons.users),
+                      label: NyaNyaLocalizations.of(context).fourPlayersLabel,
+                      content: Builder(builder: (context) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        } else if (!snapshot.hasData) {
+                          return Center(
+                              child: Text(NyaNyaLocalizations.of(context)
+                                  .unauthenticatedError));
+                        }
 
-                  return FriendDuel(
-                    displayName: user.displayName ?? '',
-                    idToken: snapshot.data!,
-                    masterServerHostname: region.masterServerHostname,
-                  );
-                }))
-          ]);
+                        return QueueAndLeaderboard(
+                          queueType: QueueType.fourPlayers,
+                          displayName: user.displayName ?? '',
+                          idToken: snapshot.data!,
+                          masterServerHostname: region.masterServerHostname,
+                        );
+                      })),
+                  BarRailTab(
+                      icon: const FaIcon(FontAwesomeIcons.peopleArrows),
+                      label: NyaNyaLocalizations.of(context).friendDuelLabel,
+                      content: Builder(builder: (context) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        } else if (!snapshot.hasData) {
+                          return Center(
+                              child: Text(NyaNyaLocalizations.of(context)
+                                  .unauthenticatedError));
+                        }
+
+                        return FriendDuel(
+                          displayName: user.displayName ?? '',
+                          idToken: snapshot.data!,
+                          masterServerHostname: region.masterServerHostname,
+                        );
+                      }))
+                ]);
+          });
     });
   }
 }
