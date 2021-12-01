fff61dae69835f8572b0b15b823e654d758a8031
CaramelDunes <dev@carameldunes.fr>
2021-12-01 22:42:31 +0100

    Set max width for device duel and lan multiplayer setup.

--- lib/screens/multiplayer/tabs/device_duel_setup.dart
+++ lib/screens/multiplayer/tabs/device_duel_setup.dart
@@ -3,6 +3,7 @@ import 'package:nyanya_rocket/localization/nyanya_localizations.dart';
 import 'package:nyanya_rocket/models/multiplayer_board.dart';
 import 'package:nyanya_rocket/screens/multiplayer/screens/local_duel.dart';
 
+import '../../../config.dart';
 import '../setup_widgets/board_picker.dart';
 
 class DeviceDuelSetup extends StatefulWidget {
@@ -26,115 +27,122 @@ class _DeviceDuelSetupState extends State<DeviceDuelSetup> {
       appBar: AppBar(
         title: Text(NyaNyaLocalizations.of(context).deviceDuelLabel),
       ),
-      body: ListView(padding: const EdgeInsets.all(8.0), children: [
-        Form(
-            key: _formState,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  NyaNyaLocalizations.of(context).durationLabel,
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                DropdownButtonFormField<Duration>(
-                  value: _duration,
-                  items: <DropdownMenuItem<Duration>>[
-                    DropdownMenuItem(
-                      child: Text(
-                          NyaNyaLocalizations.of(context).minuteCountLabel(2)),
-                      value: const Duration(minutes: 2),
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: kMaxWidthForBigScreens),
+          child: ListView(padding: const EdgeInsets.all(8.0), children: [
+            Form(
+                key: _formState,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      NyaNyaLocalizations.of(context).durationLabel,
+                      style: Theme.of(context).textTheme.headline6,
                     ),
-                    DropdownMenuItem(
-                      child: Text(
-                          NyaNyaLocalizations.of(context).minuteCountLabel(3)),
-                      value: const Duration(minutes: 3),
+                    DropdownButtonFormField<Duration>(
+                      value: _duration,
+                      items: <DropdownMenuItem<Duration>>[
+                        DropdownMenuItem(
+                          child: Text(NyaNyaLocalizations.of(context)
+                              .minuteCountLabel(2)),
+                          value: const Duration(minutes: 2),
+                        ),
+                        DropdownMenuItem(
+                          child: Text(NyaNyaLocalizations.of(context)
+                              .minuteCountLabel(3)),
+                          value: const Duration(minutes: 3),
+                        ),
+                        DropdownMenuItem(
+                          child: Text(NyaNyaLocalizations.of(context)
+                              .minuteCountLabel(4)),
+                          value: const Duration(minutes: 4),
+                        ),
+                        DropdownMenuItem(
+                          child: Text(NyaNyaLocalizations.of(context)
+                              .minuteCountLabel(5)),
+                          value: const Duration(minutes: 5),
+                        ),
+                      ],
+                      onChanged: (Duration? value) => setState(() {
+                        _duration = value ?? _duration;
+                      }),
+                      onSaved: (Duration? value) =>
+                          _duration = value ?? _duration,
                     ),
-                    DropdownMenuItem(
-                      child: Text(
-                          NyaNyaLocalizations.of(context).minuteCountLabel(4)),
-                      value: const Duration(minutes: 4),
+                    const Divider(),
+                    Text(
+                      NyaNyaLocalizations.of(context).boardLabel,
+                      style: Theme.of(context).textTheme.headline6,
                     ),
-                    DropdownMenuItem(
-                      child: Text(
-                          NyaNyaLocalizations.of(context).minuteCountLabel(5)),
-                      value: const Duration(minutes: 5),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: SizedBox(
+                        height: 150,
+                        child: BoardPicker(
+                          onChanged: (MultiplayerBoard board) {
+                            setState(() {
+                              _board = board;
+                            });
+                          },
+                        ),
+                      ),
                     ),
-                  ],
-                  onChanged: (Duration? value) => setState(() {
-                    _duration = value ?? _duration;
-                  }),
-                  onSaved: (Duration? value) => _duration = value ?? _duration,
-                ),
-                const Divider(),
-                Text(
-                  NyaNyaLocalizations.of(context).boardLabel,
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: SizedBox(
-                    height: 150,
-                    child: BoardPicker(
-                      onChanged: (MultiplayerBoard board) {
-                        setState(() {
-                          _board = board;
-                        });
+                    const Divider(),
+                    Text(
+                      NyaNyaLocalizations.of(context).nicknamesLabel,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    TextFormField(
+                      focusNode: _nicknameNodes[0],
+                      onFieldSubmitted: (_) {
+                        _nicknameNodes[0].unfocus();
+                        FocusScope.of(context).requestFocus(_nicknameNodes[1]);
+                      },
+                      decoration: InputDecoration(
+                          hintText:
+                              '${NyaNyaLocalizations.of(context).nicknameLabel} 1'),
+                      maxLength: 16,
+                      textCapitalization: TextCapitalization.words,
+                      textInputAction: TextInputAction.next,
+                      onSaved: (String? text) {
+                        _playerNames[0] = text ?? _playerNames[0];
                       },
                     ),
-                  ),
-                ),
-                const Divider(),
-                Text(
-                  NyaNyaLocalizations.of(context).nicknamesLabel,
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                TextFormField(
-                  focusNode: _nicknameNodes[0],
-                  onFieldSubmitted: (_) {
-                    _nicknameNodes[0].unfocus();
-                    FocusScope.of(context).requestFocus(_nicknameNodes[1]);
-                  },
-                  decoration: InputDecoration(
-                      hintText:
-                          '${NyaNyaLocalizations.of(context).nicknameLabel} 1'),
-                  maxLength: 16,
-                  textCapitalization: TextCapitalization.words,
-                  textInputAction: TextInputAction.next,
-                  onSaved: (String? text) {
-                    _playerNames[0] = text ?? _playerNames[0];
-                  },
-                ),
-                TextFormField(
-                  focusNode: _nicknameNodes[1],
-                  decoration: InputDecoration(
-                      hintText:
-                          '${NyaNyaLocalizations.of(context).nicknameLabel} 2'),
-                  maxLength: 16,
-                  textCapitalization: TextCapitalization.words,
-                  onSaved: (String? text) {
-                    _playerNames[1] = text ?? _playerNames[1];
-                  },
-                ),
-                Flexible(
-                  child: ElevatedButton(
-                    child: Text(NyaNyaLocalizations.of(context).playLabel),
-                    onPressed: _board != null
-                        ? () {
-                            _formState.currentState!.save();
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (BuildContext context) => LocalDuel(
-                                      board: _board!,
-                                      players: _playerNames,
-                                      duration: _duration,
-                                    )));
-                          }
-                        : null,
-                  ),
-                )
-              ],
-            )),
-      ]),
+                    TextFormField(
+                      focusNode: _nicknameNodes[1],
+                      decoration: InputDecoration(
+                          hintText:
+                              '${NyaNyaLocalizations.of(context).nicknameLabel} 2'),
+                      maxLength: 16,
+                      textCapitalization: TextCapitalization.words,
+                      onSaved: (String? text) {
+                        _playerNames[1] = text ?? _playerNames[1];
+                      },
+                    ),
+                    Flexible(
+                      child: ElevatedButton(
+                        child: Text(NyaNyaLocalizations.of(context).playLabel),
+                        onPressed: _board != null
+                            ? () {
+                                _formState.currentState!.save();
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        LocalDuel(
+                                          board: _board!,
+                                          players: _playerNames,
+                                          duration: _duration,
+                                        )));
+                              }
+                            : null,
+                      ),
+                    )
+                  ],
+                )),
+          ]),
+        ),
+      ),
     );
   }
 }
--- lib/screens/multiplayer/tabs/lan_multiplayer_setup.dart
+++ lib/screens/multiplayer/tabs/lan_multiplayer_setup.dart
@@ -9,6 +9,8 @@ import 'package:nyanya_rocket/screens/multiplayer/screens/network_multiplayer.da
 import 'package:nyanya_rocket/screens/multiplayer/setup_widgets/board_picker.dart';
 import 'package:nyanya_rocket_base/nyanya_rocket_base.dart';
 
+import '../../../config.dart';
+
 class LanMultiplayerSetup extends StatefulWidget {
   static final RegExp hostnameMatcher = RegExp(
       r'^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$');
@@ -64,182 +66,195 @@ class _LanMultiplayerSetupState extends State<LanMultiplayerSetup> {
       appBar: AppBar(
         title: const Text('LAN Multiplayer'),
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(8.0),
-        children: [
-          Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  focusNode: _nicknameFocusNode,
-                  decoration: InputDecoration(
-                      labelText: NyaNyaLocalizations.of(context).nicknameLabel),
-                  maxLength: 16,
-                  textInputAction: TextInputAction.next,
-                  textCapitalization: TextCapitalization.words,
-                  onSaved: (String? value) => _nickname = value ?? _nickname,
-                  onFieldSubmitted: (term) {
-                    _nicknameFocusNode.unfocus();
-                    FocusScope.of(context).requestFocus(_hostnameFocusNode);
-                  },
-                  validator: (String? value) {
-                    if (value != null && value.isEmpty) {
-                      return NyaNyaLocalizations.of(context)
-                          .invalidNicknameText;
-                    }
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: kMaxWidthForBigScreens),
+          child: ListView(
+            padding: const EdgeInsets.all(8.0),
+            children: [
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      focusNode: _nicknameFocusNode,
+                      decoration: InputDecoration(
+                          labelText:
+                              NyaNyaLocalizations.of(context).nicknameLabel),
+                      maxLength: 16,
+                      textInputAction: TextInputAction.next,
+                      textCapitalization: TextCapitalization.words,
+                      onSaved: (String? value) =>
+                          _nickname = value ?? _nickname,
+                      onFieldSubmitted: (term) {
+                        _nicknameFocusNode.unfocus();
+                        FocusScope.of(context).requestFocus(_hostnameFocusNode);
+                      },
+                      validator: (String? value) {
+                        if (value != null && value.isEmpty) {
+                          return NyaNyaLocalizations.of(context)
+                              .invalidNicknameText;
+                        }
 
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  focusNode: _hostnameFocusNode,
-                  initialValue: _hostname,
-                  decoration: InputDecoration(
-                    labelText: NyaNyaLocalizations.of(context).hostnameLabel,
-                  ),
-                  onSaved: (String? value) => _hostname = value ?? _hostname,
-                  validator: (String? value) {
-                    if (value == null ||
-                        !LanMultiplayerSetup.hostnameMatcher.hasMatch(value)) {
-                      return NyaNyaLocalizations.of(context)
-                          .invalidHostnameText;
-                    }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      focusNode: _hostnameFocusNode,
+                      initialValue: _hostname,
+                      decoration: InputDecoration(
+                        labelText:
+                            NyaNyaLocalizations.of(context).hostnameLabel,
+                      ),
+                      onSaved: (String? value) =>
+                          _hostname = value ?? _hostname,
+                      validator: (String? value) {
+                        if (value == null ||
+                            !LanMultiplayerSetup.hostnameMatcher
+                                .hasMatch(value)) {
+                          return NyaNyaLocalizations.of(context)
+                              .invalidHostnameText;
+                        }
 
-                    return null;
-                  },
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    child: Text(NyaNyaLocalizations.of(context).playLabel),
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        _formKey.currentState!.save();
+                        return null;
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        child: Text(NyaNyaLocalizations.of(context).playLabel),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _formKey.currentState!.save();
 
-                        InternetAddress.lookup(_hostname,
-                                type: InternetAddressType.IPv4)
-                            .then((List<InternetAddress> result) {
-                          if (result.isNotEmpty) {
-                            Navigator.of(context)
-                                .push(MaterialPageRoute(
-                                    builder: (BuildContext context) =>
-                                        NetworkMultiplayer(
-                                            nickname: _nickname,
-                                            serverAddress: result[0],
-                                            port: 43122)))
-                                .then((dynamic) {
-                              _LanMultiplayerSetupState._serverIsolate?.kill();
+                            InternetAddress.lookup(_hostname,
+                                    type: InternetAddressType.IPv4)
+                                .then((List<InternetAddress> result) {
+                              if (result.isNotEmpty) {
+                                Navigator.of(context)
+                                    .push(MaterialPageRoute(
+                                        builder: (BuildContext context) =>
+                                            NetworkMultiplayer(
+                                                nickname: _nickname,
+                                                serverAddress: result[0],
+                                                port: 43122)))
+                                    .then((dynamic) {
+                                  _LanMultiplayerSetupState._serverIsolate
+                                      ?.kill();
+                                });
+                              }
                             });
                           }
-                        });
-                      }
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Text(
-            _localIpText,
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 25),
-          ),
-          const Divider(),
-          Row(
-            children: [
-              Expanded(
-                child: DropdownButton<Duration>(
-                  value: _duration,
-                  items: <DropdownMenuItem<Duration>>[
-                    DropdownMenuItem(
-                      child: Text(
-                          NyaNyaLocalizations.of(context).minuteCountLabel(2)),
-                      value: const Duration(minutes: 2),
-                    ),
-                    DropdownMenuItem(
-                      child: Text(
-                          NyaNyaLocalizations.of(context).minuteCountLabel(3)),
-                      value: const Duration(minutes: 3),
-                    ),
-                    DropdownMenuItem(
-                      child: Text(
-                          NyaNyaLocalizations.of(context).minuteCountLabel(4)),
-                      value: const Duration(minutes: 4),
-                    ),
-                    DropdownMenuItem(
-                      child: Text(
-                          NyaNyaLocalizations.of(context).minuteCountLabel(5)),
-                      value: const Duration(minutes: 5),
+                        },
+                      ),
                     ),
                   ],
-                  onChanged: (Duration? value) => setState(() {
-                    _duration = value ?? _duration;
-                  }),
                 ),
               ),
-              const VerticalDivider(),
-              Expanded(
-                child: DropdownButton<int>(
-                  value: _playerCount,
-                  items: <DropdownMenuItem<int>>[
-                    DropdownMenuItem(
-                      child: Text(
-                          NyaNyaLocalizations.of(context).playerCountLabel(1)),
-                      value: 1,
+              Text(
+                _localIpText,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 25),
+              ),
+              const Divider(),
+              Row(
+                children: [
+                  Expanded(
+                    child: DropdownButton<Duration>(
+                      value: _duration,
+                      items: <DropdownMenuItem<Duration>>[
+                        DropdownMenuItem(
+                          child: Text(NyaNyaLocalizations.of(context)
+                              .minuteCountLabel(2)),
+                          value: const Duration(minutes: 2),
+                        ),
+                        DropdownMenuItem(
+                          child: Text(NyaNyaLocalizations.of(context)
+                              .minuteCountLabel(3)),
+                          value: const Duration(minutes: 3),
+                        ),
+                        DropdownMenuItem(
+                          child: Text(NyaNyaLocalizations.of(context)
+                              .minuteCountLabel(4)),
+                          value: const Duration(minutes: 4),
+                        ),
+                        DropdownMenuItem(
+                          child: Text(NyaNyaLocalizations.of(context)
+                              .minuteCountLabel(5)),
+                          value: const Duration(minutes: 5),
+                        ),
+                      ],
+                      onChanged: (Duration? value) => setState(() {
+                        _duration = value ?? _duration;
+                      }),
                     ),
-                    DropdownMenuItem(
-                      child: Text(
-                          NyaNyaLocalizations.of(context).playerCountLabel(2)),
-                      value: 2,
+                  ),
+                  const VerticalDivider(),
+                  Expanded(
+                    child: DropdownButton<int>(
+                      value: _playerCount,
+                      items: <DropdownMenuItem<int>>[
+                        DropdownMenuItem(
+                          child: Text(NyaNyaLocalizations.of(context)
+                              .playerCountLabel(1)),
+                          value: 1,
+                        ),
+                        DropdownMenuItem(
+                          child: Text(NyaNyaLocalizations.of(context)
+                              .playerCountLabel(2)),
+                          value: 2,
+                        ),
+                        DropdownMenuItem(
+                          child: Text(NyaNyaLocalizations.of(context)
+                              .playerCountLabel(3)),
+                          value: 3,
+                        ),
+                        DropdownMenuItem(
+                          child: Text(NyaNyaLocalizations.of(context)
+                              .playerCountLabel(4)),
+                          value: 4,
+                        ),
+                      ],
+                      onChanged: (int? value) => setState(() {
+                        _playerCount = value ?? _playerCount;
+                      }),
                     ),
-                    DropdownMenuItem(
-                      child: Text(
-                          NyaNyaLocalizations.of(context).playerCountLabel(3)),
-                      value: 3,
-                    ),
-                    DropdownMenuItem(
-                      child: Text(
-                          NyaNyaLocalizations.of(context).playerCountLabel(4)),
-                      value: 4,
-                    ),
-                  ],
-                  onChanged: (int? value) => setState(() {
-                    _playerCount = value ?? _playerCount;
-                  }),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 150,
+                child: BoardPicker(
+                  onChanged: (MultiplayerBoard board) {
+                    setState(() {
+                      _board = board;
+                    });
+                  },
+                ),
+              ),
+              Center(
+                child: ElevatedButton(
+                  child: Text(NyaNyaLocalizations.of(context).createLabel),
+                  onPressed: _board == null
+                      ? null
+                      : () {
+                          _LanMultiplayerSetupState._serverIsolate?.kill();
+
+                          Isolate.spawn<_ArgumentBundle>(
+                                  _LanMultiplayerSetupState.serverEntryPoint,
+                                  _ArgumentBundle(
+                                      _board!.board(), _playerCount))
+                              .then((Isolate isolate) =>
+                                  _LanMultiplayerSetupState._serverIsolate =
+                                      isolate);
+
+                          _hostname = '10.0.2.2';
+                        },
                 ),
               ),
             ],
           ),
-          SizedBox(
-            height: 150,
-            child: BoardPicker(
-              onChanged: (MultiplayerBoard board) {
-                setState(() {
-                  _board = board;
-                });
-              },
-            ),
-          ),
-          Center(
-            child: ElevatedButton(
-              child: Text(NyaNyaLocalizations.of(context).createLabel),
-              onPressed: _board == null
-                  ? null
-                  : () {
-                      _LanMultiplayerSetupState._serverIsolate?.kill();
-
-                      Isolate.spawn<_ArgumentBundle>(
-                              _LanMultiplayerSetupState.serverEntryPoint,
-                              _ArgumentBundle(_board!.board(), _playerCount))
-                          .then((Isolate isolate) => _LanMultiplayerSetupState
-                              ._serverIsolate = isolate);
-
-                      _hostname = '10.0.2.2';
-                    },
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
